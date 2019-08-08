a7bb44f0d743a6f5bf9270c4fadabe122db19fb8
verity <verity.honebon@royalacademy.org.uk>
2019-08-08 17:04:55 +0100

    integrate rails and react routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
     resources :todos
   end
 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
