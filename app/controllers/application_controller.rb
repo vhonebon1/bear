3f679d86e6180cd3c6f09c2b5470a1b5095e96c9
verity <verity.honebon@royalacademy.org.uk>
2019-08-08 17:45:45 +0100

    routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    render :file => '/todo-app/public/index.html'
+    render :file => 'todo-app/public/index.html'
   end
 end
