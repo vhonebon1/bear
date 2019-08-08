6c8c380a52248564a1523d01eb5f739c96a7dfd2
verity <verity.honebon@royalacademy.org.uk>
2019-08-08 17:55:48 +0100

    more routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::API
-  def fallback_index_html
-    render :file => 'todo-app/public/index.html'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :todos
   end
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  get '*path', to: "static_pages#index", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 end
