f496648e72ddfa4c2f621dfd82478c91b57112f8
verity <verity.honebon@royalacademy.org.uk>
2019-08-15 16:24:50 +0100

    finish modal functionality

--- todo-app/src/components/VideoPlayer.jsx
+++ todo-app/src/components/VideoPlayer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
+import IosClose from 'react-ionicons/lib/IosClose'
 
 const VideoPlayer = ({ selectedVideo, onEnded, handleClearSelected }) =>
   <div className="video-wrapper">
     <div className="video-inner">
       <div className="video-close">
-        <img onClick={() => handleClearSelected()} src="./close.svg" />
+        <IosClose onClick={() => handleClearSelected()} fontSize="40px" color="#ffffff" />
       </div>
       <ReactPlayer
         url={`https://www.youtube.com/watch?v=${selectedVideo}&start_radio=1&list=RDDDWKuo3gXMQ`}
